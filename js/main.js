e0634d865b162bfc94b359b10fd0f8ad385e4fea
boedegoat <bhremada.fka@gmail.com>
2021-07-22 17:08:55 +0700

    optimize js

--- js/main.js
+++ js/main.js
@@ -2,40 +2,53 @@ const inputs = document.querySelectorAll('.input')
 const tipAmountDisplay = document.getElementById('tip-amount-display')
 const totalDisplay = document.getElementById('total-display')
 const btnReset = document.querySelector('.btn-reset')
+const selectTipItems = document.querySelectorAll('#select-tip-list .item:not(.custom)')
+const customTipInput = document.getElementById('custom-tip')
 
-let appObject
-
-initAppObject()
-
-function initAppObject() {
-  appObject = {
-    bill: 0,
-    tip: 0,
-    people: 0,
-  }
+const appObject = {
+  bill: 0,
+  tip: 0,
+  people: 0,
 }
 
+inputs.forEach((input) => {
+  input.addEventListener('input', (e) => {
+    appObject[e.target.name] = +e.target.value
+    if (e.target.name === 'tip') {
+      resetSelectedTip()
+    }
+    displayResult()
+  })
+})
+
 btnReset.addEventListener('click', () => {
-  initAppObject()
+  resetAppObject()
   resetDisplay()
+  resetSelectedTip()
   inputs.forEach((input) => {
     input.value = ''
   })
 })
 
-inputs.forEach((input) => {
-  input.addEventListener('input', (e) => {
-    appObject[e.target.name] = +e.target.value
-
-    if (!appObjectIsFilled()) {
-      return resetDisplay()
-    }
-
-    const [tipAmount, total] = calculateResult()
-    tipAmountDisplay.innerHTML = tipAmount.toString()
-    totalDisplay.innerHTML = total.toString()
+selectTipItems.forEach((item) => {
+  item.addEventListener('click', (e) => {
+    resetSelectedTip()
+    customTipInput.value = ''
+    e.target.classList.add('selected')
+    appObject.tip = e.target.value
+    displayResult()
   })
 })
+
+function displayResult() {
+  if (!appObjectIsFilled()) {
+    return resetDisplay()
+  }
+
+  const [tipAmount, total] = calculateResult()
+  tipAmountDisplay.innerHTML = tipAmount.toString()
+  totalDisplay.innerHTML = total.toString()
+}
 
 function calculateResult() {
   const { bill, tip, people } = appObject
@@ -52,7 +65,19 @@ function appObjectIsFilled() {
   return Object.values(appObject).every((value) => value)
 }
 
+function resetAppObject() {
+  Object.keys(appObject).forEach((property) => {
+    appObject[property] = 0
+  })
+}
+
 function resetDisplay() {
   tipAmountDisplay.innerHTML = '0.00'
   totalDisplay.innerHTML = '0.00'
+}
+
+function resetSelectedTip() {
+  selectTipItems.forEach((item) => {
+    item.classList.remove('selected')
+  })
 }
